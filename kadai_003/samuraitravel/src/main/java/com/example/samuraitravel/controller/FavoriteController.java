d631eb1d8b722f2b04fef59d8770d600bcdc9755
cubeorange4 <cubeorange4@gmail.com>
2024-01-07 10:03:08 +0900

    課題追加

--- kadai_003/samuraitravel/src/main/java/com/example/samuraitravel/controller/FavoriteController.java
+++ kadai_003/samuraitravel/src/main/java/com/example/samuraitravel/controller/FavoriteController.java
@@ -5,8 +5,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,6 +20,7 @@ import com.example.samuraitravel.repository.HouseRepository;
 import com.example.samuraitravel.security.UserDetailsImpl;
 import com.example.samuraitravel.service.FavoriteService;
 
+@Controller
 public class FavoriteController {
 	private final FavoriteRepository favoriteRepository;
 	private final HouseRepository houseRepository;
@@ -41,32 +42,29 @@ public class FavoriteController {
 		return "favorite/index";
 	}
 	
-	@PostMapping("house/{houseId}/favorite/create")
+	@PostMapping("/houses/{houseId}/favorite/create")
 	public String create(@PathVariable(name = "houseId") Integer houseId,
 						 @AuthenticationPrincipal UserDetailsImpl userDetailsImpl,
-						 BindingResult bindingResult,
+						 //BindingResult bindingResult,
 						 RedirectAttributes redirectAttributes,
 						 Model model) 
 	{
 		House house = houseRepository.getReferenceById(houseId);
 		User user = userDetailsImpl.getUser();
 		
-		if(bindingResult.hasErrors()) {
-			return "houses/{houseId}";
-		}
-		
 		favoriteService.create(house, user);
+        redirectAttributes.addFlashAttribute("successMessage", "お気に入りに追加しました。");    
 		
-		return "redirect:/house/{houseId}";
+		return "redirect:/houses/{houseId}";
 	}
 	
-	@PostMapping("house/{houseId}/favorite/{favoriteId}/delete")
+	@PostMapping("/houses/{houseId}/favorite/{favoriteId}/delete")
 	public String delete(@PathVariable(name = "favoriteId") Integer favoriteId, RedirectAttributes redirectAttributes) {
 		favoriteRepository.deleteById(favoriteId);
 		
 		redirectAttributes.addFlashAttribute("successMessage", "お気に入りを削除しました。");
 		
-		return "redirect:/house/{houseId}";
+		return "redirect:/houses/{houseId}";
 	}
 	
 	
